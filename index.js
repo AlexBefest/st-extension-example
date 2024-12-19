d1fa9891ee74f2bb5817ad9f2bdb26e5c11285c6
AlexBefest <sasha11035@yandex.ru>
2024-12-19 22:19:21 +0300

    123

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // The main script for the extension
 // The following are examples of some basic extension functionality
 
-//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
+// You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
 import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
 
-//You'll likely need to import some other functions from the main script
+// You'll likely need to import some other functions from the main script
 import { saveSettingsDebounced } from "../../../../script.js";
 
 // Keep track of where your extension is located, name should match repo name
@@ -13,10 +13,9 @@ const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
 const defaultSettings = {};
 
-
 // Loads the extension settings if they exist, otherwise initializes them to the defaults.
 async function loadSettings() {
-  //Create the settings if they don't exist
+  // Create the settings if they don't exist
   extension_settings[extensionName] = extension_settings[extensionName] || {};
   if (Object.keys(extension_settings[extensionName]).length === 0) {
     Object.assign(extension_settings[extensionName], defaultSettings);
@@ -44,6 +43,24 @@ function onSummarizeButtonClick() {
   );
 }
 
+// This function gets the last message from the chat and sets it in the summary input field
+function setLastMessageInSummaryInput() {
+  const context = getContext();
+  const chat = context.chat;
+
+  if (!chat || chat.length === 0) {
+    console.debug('No messages in chat');
+    return;
+  }
+
+  const lastMessage = chat[chat.length - 1];
+  if (lastMessage && lastMessage.mes) {
+    $("#summary_input").val(lastMessage.mes).trigger("input");
+  } else {
+    console.debug('Last message does not contain text');
+  }
+}
+
 // This function is called when the extension is loaded
 jQuery(async () => {
   // This is an example of loading HTML from a file
@@ -60,4 +77,11 @@ jQuery(async () => {
 
   // Load settings when starting things up (if you have any)
   loadSettings();
+
+  // Add a button to get the last message
+  const getLastMessageButton = $('<button id="get_last_message_button" class="menu_button">Get Last Message</button>');
+  $(".example-extension_block:first").after(getLastMessageButton);
+
+  // Add event listener for the new button
+  $("#get_last_message_button").on("click", setLastMessageInSummaryInput);
 });
