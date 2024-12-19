ef8fb2492a452dfbf21b299a89bdc40aada02c1c
AlexBefest <sasha11035@yandex.ru>
2024-12-19 22:01:52 +0300

    78

--- index.js
+++ index.js
@@ -1,76 +1,63 @@
-import { extension_settings, getContext, saveSettingsDebounced } from "../../../extensions.js";
-import { generateRaw } from "../../../../script.js";
+// The main script for the extension
+// The following are examples of some basic extension functionality
 
-const extensionName = "my-summary-extension";
+//You'll likely need to import extension_settings, getContext, and loadExtensionSettings from extensions.js
+import { extension_settings, getContext, loadExtensionSettings } from "../../../extensions.js";
+
+//You'll likely need to import some other functions from the main script
+import { saveSettingsDebounced } from "../../../../script.js";
+
+// Keep track of where your extension is located, name should match repo name
+const extensionName = "st-extension-example";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
-const defaultSettings = {
-    prompt: "Ignore previous instructions. Summarize the most important facts and events in the story so far. Limit the summary to {{words}} words or less. Your response should include nothing but the summary.",
-    words: 200,
-};
+const extensionSettings = extension_settings[extensionName];
+const defaultSettings = {};
 
-function loadSettings() {
-    extension_settings[extensionName] = extension_settings[extensionName] || {};
-    if (Object.keys(extension_settings[extensionName]).length === 0) {
-        Object.assign(extension_settings[extensionName], defaultSettings);
-    }
 
-    $("#my_summary_prompt").val(extension_settings[extensionName].prompt).trigger("input");
-    $("#my_summary_words").val(extension_settings[extensionName].words).trigger("input");
-    $("#my_summary_words_value").text(extension_settings[extensionName].words);
+ 
+// Loads the extension settings if they exist, otherwise initializes them to the defaults.
+async function loadSettings() {
+  //Create the settings if they don't exist
+  extension_settings[extensionName] = extension_settings[extensionName] || {};
+  if (Object.keys(extension_settings[extensionName]).length === 0) {
+    Object.assign(extension_settings[extensionName], defaultSettings);
+  }
+
+  // Updating settings in the UI
+  $("#example_setting").prop("checked", extension_settings[extensionName].example_setting).trigger("input");
 }
 
-function onSummaryPromptInput(event) {
-    const value = $(event.target).val();
-    extension_settings[extensionName].prompt = value;
-    saveSettingsDebounced();
+// This function is called when the extension settings are changed in the UI
+function onExampleInput(event) {
+  const value = Boolean($(event.target).prop("checked"));
+  extension_settings[extensionName].example_setting = value;
+  saveSettingsDebounced();
 }
 
-function onSummaryWordsInput(event) {
-    const value = $(event.target).val();
-    extension_settings[extensionName].words = Number(value);
-    $("#my_summary_words_value").text(extension_settings[extensionName].words);
-    saveSettingsDebounced();
+// This function is called when the button is clicked
+function onButtonClick() {
+  // You can do whatever you want here
+  // Let's make a popup appear with the checked setting
+  toastr.info(
+    `The checkbox is ${extension_settings[extensionName].example_setting ? "checked" : "not checked"}`,
+    "A popup appeared because you clicked the button!"
+  );
 }
 
-async function forceSummarizeChat() {
-    const context = getContext();
-    const chat = context.chat;
-
-    if (!context.chatId) {
-        toastr.warning('No chat selected');
-        return '';
-    }
-
-    const toast = toastr.info('Summarizing chat...', 'Please wait', { timeOut: 0, extendedTimeOut: 0 });
-    const prompt = substituteParamsExtended(extension_settings[extensionName].prompt, { words: extension_settings[extensionName].words });
-
-    try {
-        const summary = await generateRaw(chat.map(m => m.mes).join('\n'), '', false, false, prompt);
-        $("#my_summary_contents").val(summary);
-        toastr.clear(toast);
-    } catch (error) {
-        toastr.error(String(error), 'Failed to summarize text');
-        console.log(error);
-        toastr.clear(toast);
-    }
-}
-
-function setupListeners() {
-    $("#my_summary_prompt").off('input').on('input', onSummaryPromptInput);
-    $("#my_summary_words").off('input').on('input', onSummaryWordsInput);
-    $("#my_summary_force_summarize").off('click').on('click', forceSummarizeChat);
-}
-
+// This function is called when the extension is loaded
 jQuery(async () => {
-    const settingsHtml = await $.get(`${extensionFolderPath}/settings.html`);
-    $("#extensions_settings").append(settingsHtml);
-    setupListeners();
-    loadSettings();
-});
+  // This is an example of loading HTML from a file
+  const settingsHtml = await $.get(`${extensionFolderPath}/example.html`);
 
-function substituteParamsExtended(template, params) {
-    for (const [key, value] of Object.entries(params)) {
-        template = template.replace(new RegExp(`{{${key}}}`, 'g'), value);
-    }
-    return template;
-}
+  // Append settingsHtml to extensions_settings
+  // extension_settings and extensions_settings2 are the left and right columns of the settings menu
+  // Left should be extensions that deal with system functions and right should be visual/UI related 
+  $("#extensions_settings").append(settingsHtml);
+
+  // These are examples of listening for events
+  $("#my_button").on("click", onButtonClick);
+  $("#example_setting").on("input", onExampleInput);
+
+  // Load settings when starting things up (if you have any)
+  loadSettings();
+});
