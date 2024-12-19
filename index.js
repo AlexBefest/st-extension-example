0da6e9e0030c505a6881cc0ca3ac64ac5fd46360
AlexBefest <sasha11035@yandex.ru>
2024-12-19 22:45:21 +0300

    11

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ import { saveSettingsDebounced, generateRaw } from "../../../../script.js";
 const extensionName = "st-extension-example";
 const extensionFolderPath = `scripts/extensions/third-party/${extensionName}`;
 const extensionSettings = extension_settings[extensionName];
-const defaultSettings = {};
+const defaultSettings = {
+    chunk_size: 20,
+};
 
 // Loads the extension settings if they exist, otherwise initializes them to the defaults.
 async function loadSettings() {
@@ -23,6 +25,8 @@ async function loadSettings() {
 
   // Updating settings in the UI
   $("#summary_input").val(extension_settings[extensionName].summary_input).trigger("input");
+  $("#chunk_size").val(extension_settings[extensionName].chunk_size).trigger("input");
+  $("#chunk_size_value").text(extension_settings[extensionName].chunk_size);
 }
 
 // This function is called when the extension settings are changed in the UI
@@ -32,36 +36,15 @@ function onSummaryInput(event) {
   saveSettingsDebounced();
 }
 
-// This function is called when the button is clicked
-function onSummarizeButtonClick() {
-  // You can do whatever you want here
-  // Let's make a popup appear with the summary text
-  const summaryText = $("#summary_input").val();
-  toastr.info(
-    `Summary: ${summaryText}`,
-    "A popup appeared because you clicked the Summarize button!!!!"
-  );
+// This function is called when the chunk size slider is changed
+function onChunkSizeChange(event) {
+  const value = parseInt($(event.target).val());
+  extension_settings[extensionName].chunk_size = value;
+  $("#chunk_size_value").text(value);
+  saveSettingsDebounced();
 }
 
-// This function gets the last message from the chat and sets it in the summary input field
-function setLastMessageInSummaryInput() {
-  const context = getContext();
-  const chat = context.chat;
-
-  if (!chat || chat.length === 0) {
-    console.debug('No messages in chat');
-    return;
-  }
-
-  const lastMessage = chat[chat.length - 1];
-  if (lastMessage && lastMessage.mes) {
-    $("#summary_input").val(lastMessage.mes).trigger("input");
-  } else {
-    console.debug('Last message does not contain text');
-  }
-}
-
-// This function sends the entire chat to the neural network in chunks of 20 messages and sets the response in the summary input field
+// This function sends the entire chat to the neural network in chunks and sets the response in the summary input field
 async function sendEntireChatToNeuralNetwork() {
   const context = getContext();
   const chat = context.chat;
@@ -71,7 +54,7 @@ async function sendEntireChatToNeuralNetwork() {
     return;
   }
 
-  const chunkSize = 20;
+  const chunkSize = extension_settings[extensionName].chunk_size;
   let summaries = [];
 
   for (let i = 0; i < chat.length; i += chunkSize) {
@@ -107,23 +90,10 @@ jQuery(async () => {
   $("#extensions_settings").append(settingsHtml);
 
   // These are examples of listening for events
-  $("#summarize_button").on("click", onSummarizeButtonClick);
   $("#summary_input").on("input", onSummaryInput);
+  $("#chunk_size").on("input", onChunkSizeChange);
+  $("#send_to_neural_network_button").on("click", sendEntireChatToNeuralNetwork);
 
   // Load settings when starting things up (if you have any)
   loadSettings();
-
-  // Add a button to get the last message
-  const getLastMessageButton = $('<button id="get_last_message_button" class="menu_button">Get Last Message</button>');
-  $(".example-extension_block:first").after(getLastMessageButton);
-
-  // Add event listener for the new button
-  $("#get_last_message_button").on("click", setLastMessageInSummaryInput);
-
-  // Add a button to send the entire chat to the neural network
-  const sendToNeuralNetworkButton = $('<button id="send_to_neural_network_button" class="menu_button">Send Entire Chat to Neural Network</button>');
-  getLastMessageButton.after(sendToNeuralNetworkButton);
-
-  // Add event listener for the new button
-  $("#send_to_neural_network_button").on("click", sendEntireChatToNeuralNetwork);
 });
